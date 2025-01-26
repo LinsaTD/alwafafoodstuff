7ec6590903d5d339db1f8ac6589c5a2f9e031232
Linsa T D <99024903+LinsaTD@users.noreply.github.com>
2025-01-26 14:42:46 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ $(function () {
       $('#navbarExample01').toggleClass('active-class');
   });
 });
+document.querySelectorAll('.nav-link').forEach(link => {
+  link.addEventListener('click', event => {
+    const targetId = event.target.getAttribute('href').split('#')[1];
+    const targetElement = document.getElementById(targetId);
+    const navbarHeight = document.querySelector('nav').offsetHeight;
+    window.scrollTo({ top: targetElement.offsetTop - navbarHeight, behavior: 'smooth' });
+  });
+});
